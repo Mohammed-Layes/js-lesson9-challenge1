c54898783f852b4c65a36a0f671a4faa8bcaf810
Mohammed.Layes <mohammed.layes@gmail.com>
2023-03-20 02:23:58 -0400

    Assign potluck dish

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// assign button
+const assignButton = document.querySelector(".assign");
+// list that populates guest's name and their assigned dish
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function() {
     const guest = guestInput.value;
@@ -43,3 +47,17 @@ const updateGuestCount = function() {
         guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function() {
+    let potluckItems = ["Halloumi and Melon Skewers", "Veggie Nori Rolls", "Tangy Carrot Slaw", "Pesto Pasta Salad", "Lemon Cream Icebox Cake", "Chilled Cucumber Noodles with Sesame Dressing", "3-Bean Israeli Couscous Salad", "Zucchini Noodles with Tomatoes and Corn", "Tuna Salad", "Egg Salad", "Avocado Squares", "Key Lime Pie"];
+};
+
+const allGuests = document.querySelectorAll(".guest-list li");
+
+for (let guest of allGuests) {
+    const randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    const randomPotluckItem = potluckItems[randomPotluckIndex];
+    const listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}`;
+    assignedItems.append(listItem);
+}
